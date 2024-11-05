b734854e89215484c80c8fb13a9f08811298ae31
Byttemos <hriskaer@gmail.com>
2024-11-05 12:55:03 +0100

    write changes to calendar file

--- CheckLocalChanges.py
+++ CheckLocalChanges.py
@@ -4,21 +4,46 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler 
 import os
 from pathlib import Path
+import shutil
+import difflib
 
 
-def get_entry_type(event):
-    if event.src_path == str(home) + '/.calcurse/apts':
-        return 'apts'
-    if event.src_path == str(home) + '/.calcurse/todo':
-        return 'todo'
 
+def get_diffs(event):
+    '''compare contents of modified calendar file with reference backup file'''
+    modified_src = event.src_path
+    if modified_src[-4:] == 'apts':
+        with open(modified_src, 'r') as modified, open(apt_lastupdate_src, 'r') as lastupdate:
+
+            diff = difflib.unified_diff(
+                modified.readlines(),
+                lastupdate.readlines(),
+            )
+        for line in diff:
+            print(line)
+            
+    if modified_src[-4:] == 'todo':
+        #print('\n'.join(diff))
+        print('todo')
+    
 def on_modified(event):
+    '''Filter event types that arent modifications from the eventhandler trigger'''
     if event.event_type == 'modified':
-        print(get_entry_type(event))
-        
+        get_diffs(event)
 
 if __name__ == '__main__':
     home = Path.home()
+    if os.path.exists(str(home) + '/.calcurse/apt_lastupdate'):
+        apt_lastupdate_src = str(home) + '/.calcurse/apt_lastupdate' 
+    else:
+        apt_lastupdate_src = shutil.copyfile(str(home) + '/.calcurse/apts', str(home) + '/.calcurse/apt_lastupdate')
+
+    if os.path.exists(str(home) + '/.calcurse/todo_lastupdate'):
+        todo_lastupdate_src = str(home) + '/.calcurse/todo_lastupdate' 
+    else:
+        todo_lastupdate_src = shutil.copyfile(str(home) + '/.calcurse/todo', str(home) + '/.calcurse/todo_lastupdate')
+
+
     path = sys.argv[1] if len(sys.argv) > 1 else str(home) + '/.calcurse/'
     event_handler = FileSystemEventHandler()
     event_handler.on_modified = on_modified
